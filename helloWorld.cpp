9a451c4aa342bfeffbe1d222f408f8a26cfed804
strikerEng <kyrheepowell@gmail.com>
2023-02-16 20:51:58 -0800

    Hello world program

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+// Include the input/output stream library
+#include <iostream>
+
+// 
+using namespace std;
+
+int main(){
+    cout << "hello, world" << endl;
+    return 0;
+}
+
+// To compile: g++ -o ./helloWorld(output name) helloWorld.cpp(source file)
