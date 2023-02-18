7866e5275b0af72e3f490807f448712da853ad8d
strikerEng <kyrheepowell@gmail.com>
2023-02-18 10:27:00 -0800

    Imported the string standard library

--- storeAndDisplayUserInput.cpp
+++ storeAndDisplayUserInput.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
+#include<string>
 
 int main(){
     /* variable declaration */
     double x, d;
     int i;
     char c;
-
-    /* string s; is not a valid variable declaration unless I define a string type, 
-       but user-defined types usually start with an upper cased letter.
-    */
+    std::string s;
 
     /* get user input */
     std::cin >> x;
@@ -17,7 +15,7 @@ int main(){
     std::cout << "x: " << x << std::endl;
 
     /* display value of variables*/
-    std::cout << "d: " << d << " i: " << i << " c: " << c << std::endl;
+    std::cout << "d: " << d << " i: " << i << " c: " << c << " s: " << s << std::endl;
 
     return 0;
 }
