30e459ac85cb406b1fc4cfcbc0735b0758b348d4
strikerEng <kyrheepowell@gmail.com>
2023-02-18 10:29:28 -0800

    displaying expression values

---
+++ expressionsValueAndType.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::cout << 2 + 3 << std::endl;
+    std::cout << 19 / 5 << std::endl;
+    std::cout << 19.0 / 5 << std::endl;
+    std::cout << 3 * 6.0 << std::endl;
+    std::cout << 19 % 5 << std::endl;
+    std::cout << 2 % 7 << std::endl;
+    return 0;
+}
