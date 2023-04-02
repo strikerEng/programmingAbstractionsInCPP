7c679c0ce4ea1514ad943599bd44e62c89f8c40d
strikerEng <kyrheepowell@gmail.com>
2023-04-02 11:10:52 -0700

    Minor refactoring to handle user input

--- firstNOddIntegers.cpp
+++ firstNOddIntegers.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 
 int main(){
-    int max = 4;
     int res = 0;
-    int oddIntegerFound = 0;
+
+    // count the number of odd integers found
+    int maxOddIntegersToFind;
+    
+    std::cin >> maxOddIntegersToFind; /* store user input*/
+    const int max = maxOddIntegersToFind;
 
 
     // make sure max is a positive integer
-    if ( max < 0 ) {
+    if ( maxOddIntegersToFind <= 0 ) {
         return 1;
     }
 
-    // i is less than the max
-    for(int i = 1; oddIntegerFound < max ; i++){
+    for(int i = 1; maxOddIntegersToFind > 0 ; i++){
 
         // i is odd
         if ( i % 2 == 1 ){
-            oddIntegerFound ++;
+            maxOddIntegersToFind --;
             res += i;
         }
         
