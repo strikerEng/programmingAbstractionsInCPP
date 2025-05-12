0f1b8666ec4ffe8bde0164c00364fe61260cec79
strikerEng <kyrheepowell@gmail.com>
2025-05-11 20:43:39 -0700

    style(firstNOddIntegers): Style changes to file.

--- firstNOddIntegers.cpp
+++ firstNOddIntegers.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 
-int main(){
+int main()
+{
     int res = 0;
 
     // count the number of odd integers found
     int maxOddIntegersToFind;
-    
+
     std::cin >> maxOddIntegersToFind; /* store user input*/
     const int max = maxOddIntegersToFind;
 
-
     // make sure max is a positive integer
-    if ( maxOddIntegersToFind <= 0 ) {
+    if (maxOddIntegersToFind <= 0)
+    {
         return 1;
     }
 
-    for(int i = 1; maxOddIntegersToFind > 0 ; i++){
+    for (int i = 1; maxOddIntegersToFind > 0; i++)
+    {
 
         // i is odd
-        if ( i % 2 == 1 ){
-            maxOddIntegersToFind --;
+        if (i % 2 == 1)
+        {
+            maxOddIntegersToFind--;
             res += i;
         }
-        
     }
-
     std::cout << "Sum of the first " << max << " odd integers " << res << std::endl;
-    
 }
